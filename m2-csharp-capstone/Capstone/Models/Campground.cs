5c8849e5d8404e256dcb5f05495972a1d0f106b5
Rebekah Sherman <dirtbeans@gmail.com>
2018-06-28 11:08:36 -0400

    models

--- m2-csharp-capstone/Capstone/Models/Campground.cs
+++ m2-csharp-capstone/Capstone/Models/Campground.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Capstone.Models
 {
-    class Campground
+    public class Campground
     {
+        public int campground_id { get; set; }
+        public int park_id { get; set; }
+        public string name { get; set; }
+        public int open_from_mm { get; set; }
+        public int open_to_mm { get; set; }
+        public decimal daily_fee { get; set; }
+
+
     }
 }
--- m2-csharp-capstone/Capstone/Models/Park.cs
+++ m2-csharp-capstone/Capstone/Models/Park.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Capstone.Models
 {
-    class Park
+    public class Park
     {
+        public int park_id { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+        public string establish_date { get; set; } //date string or int?    
+        public int area { get; set; }
+        public int visitors { get; set; }
+        public string description { get; set; }
     }
 }
