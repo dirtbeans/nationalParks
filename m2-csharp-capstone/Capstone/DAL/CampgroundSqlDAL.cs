9aa7ec51b27eeada922e649d9016e5d961fc6d35
Rebekah Sherman <dirtbeans@gmail.com>
2018-06-28 14:35:07 -0400

    campground DAL

--- m2-csharp-capstone/Capstone/DAL/CampgroundSqlDAL.cs
+++ m2-csharp-capstone/Capstone/DAL/CampgroundSqlDAL.cs
@@ -4,16 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Capstone.Models;
+using System.Data.SqlClient;
+using System.IO;
 
 namespace Capstone.DAL
 {
     public class CampgroundSqlDAL
     {
+        private string connectionString;
+        private const string SQL_GetAllCampgrounds = @"SELECT * FROM campground"; 
+
+        public CampgroundSqlDAL(string connectionString)
+        {
+            this.connectionString = connectionString; 
+        }
+
         public List<Campground> GetAllCampgrounds()
         {
             List<Campground> result = new List<Campground>();
 
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_GetAllCampgrounds, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        
+                        Campground c = new Campground();
+                        
+                        c.Name = Convert.ToString(reader["name"]);
+                        c.Open_from_mm = Convert.ToInt32(reader["open_from_mn"]); 
+                        c.Open_to_mm = Convert.ToInt32(reader["close_from_mn"]); 
+                        c.Daily_fee = Convert.ToDecimal(reader["daily_fee"]); 
+                      
+
+                        result.Add(c);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
             return result;
         }
     }
+    
 }
